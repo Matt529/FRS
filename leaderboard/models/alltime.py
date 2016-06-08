4fb2a0df7ef937bb2620728181e97c16fa1de2c2
Justin Tervay <jtervay97@gmail.com>
2016-06-08 19:52:31 -0400

    Shaved off a ton of time on match wins / win rate per team calculations

--- leaderboard/models/alltime.py
+++ leaderboard/models/alltime.py
@@ -2,7 +2,7 @@ from itertools import combinations
 
 from TBAW.models import Match, Team
 from collections import Counter
-from operator import itemgetter
+from django.db.models import Count, F, ExpressionWrapper, FloatField
 from util.check import event_has_f3_match
 
 
@@ -59,21 +59,7 @@ class AllianceLeaderboard:
 class TeamLeaderboard:
     @staticmethod
     def most_match_wins(n=None):
-        matches = Match.objects.filter(winner__isnull=False)
-        count = Counter()
-        for m in matches:
-            for team in m.winner.teams.all():
-                count[team] += 1
-
-        return count.most_common() if n is None else count.most_common(n)
-
-    @staticmethod
-    def most_match_wins_x(n=None):
-        d = {}
-        for team in Team.objects.filter(alliance__isnull=False):
-            d[team] = Match.objects.filter(winner__in=team.alliance_set.all()).count()
-
-        return sorted(d.items(), key=itemgetter(1))[:n]
+        return Team.objects.annotate(num_wins=Count('alliance__winner')).order_by('-num_wins')[:n]
 
     @staticmethod
     def most_event_wins(n=None):
@@ -82,7 +68,8 @@ class TeamLeaderboard:
         for match in matches_of_3:
             for team in match.winner.teams.all():
                 count[team] += 1
-        matches_of_2 = [x for x in Match.objects.filter(comp_level__exact='f', match_number__exact=2, winner__isnull=False) if
+        matches_of_2 = [x for x in
+                        Match.objects.filter(comp_level__exact='f', match_number__exact=2, winner__isnull=False) if
                         not event_has_f3_match(x.event.key)]
         for match in matches_of_2:
             for team in match.winner.teams.all():
@@ -92,21 +79,14 @@ class TeamLeaderboard:
 
     @staticmethod
     def highest_win_rate(n=None):
-        matches = Match.objects.filter(winner__isnull=False)
-        wins = Counter()
-        total = Counter()
-        for match in matches:
-            for alliance in match.alliances.all():
-                for team in alliance.teams.all():
-                    total[team] += 1
-
-            for team in match.winner.teams.all():
-                wins[team] += 1
-
-        for key, value in total.most_common():
-            if total[key] >= 14:
-                total[key] = wins[key] / total[key]
-            else:
-                del total[key]
-
-        return total.most_common() if n is None else total.most_common(n)
+        """
+        1. Counts how many times a team has appeared on an alliance and saves it to a float named num_played
+        2. Counts how many times a team has appeared on a winning alliance and saves it to a float named num_wins
+        3. Divides num_wins by num_played and saves it to a float named win_rate
+        4. Orders the teams by win_rate
+        """
+        return Team.objects.annotate(
+            num_played=ExpressionWrapper(Count('alliance__match', distinct=True), output_field=FloatField()),
+            num_wins=ExpressionWrapper(Count('alliance__winner', distinct=True), output_field=FloatField())).annotate(
+            win_rate=ExpressionWrapper(F('num_wins') * 1.0 / F('num_played'), output_field=FloatField())).order_by(
+            '-win_rate')[:n]
--- util/management/commands/resetelo.py
+++ util/management/commands/resetelo.py
@@ -13,3 +13,7 @@ class Command(BaseCommand):
                 print("reset {0}".format(t))
             except UnicodeEncodeError:
                 print("reset {0}".format(t).encode('utf-8'))
+        # with open('elo.txt', 'w', encoding='utf-8') as file:
+        #     file.write("Rank\tTeam\tElo (initial 25)\n")
+        #     for count, team in enumerate(Team.objects.filter(event__year=2016).distinct().order_by('-elo_mu'), start=1):
+        #         file.write("{0}\t{1}\t{2}\n".format(count, team.__str__().replace('\t', ''), team.elo_mu * 60).replace("\"", '\'\''))
