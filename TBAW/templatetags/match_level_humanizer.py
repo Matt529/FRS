1c8f8fc965a6c9e2afb2f192d15fc43553f1e1b0
Justin Tervay <jtervay97@gmail.com>
2016-06-20 20:32:19 -0400

    Add humanizer for match list strings

---
+++ TBAW/templatetags/match_level_humanizer.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+comp_level_dict = {
+    'qm': 'Qualifying',
+    'ef': 'Eighth-final',
+    'qf': 'Quarterfinal',
+    'sf': 'Semifinal',
+    'f': 'Final',
+}
+
+
+@register.filter(name='match_humanize')
+def match_humanize(value):
+    return comp_level_dict.get(value)
