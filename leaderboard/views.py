38a27dcd668fa98b1489caa316ddfdbb95003692
Justin Tervay <jtervay97@gmail.com>
2016-06-20 21:05:12 -0400

    Add rank, min_elo, max_elo to Elo leaderboard

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,9 +1,12 @@
+from django.db.models import F
 from django.shortcuts import render
 from .models import TeamLeaderboard
 
 
 def leaderboard(request):
+    annotated_queryset = TeamLeaderboard.highest_elo(100).annotate(elo_max=F('elo_mu') + F('elo_sigma'),
+                                                                   elo_min=F('elo_mu') - F('elo_sigma'))
     return render(request, 'leaderboard.html',
                   context={
-                      'elo_leaders': TeamLeaderboard.highest_elo(100)
+                      'elo_leaders': annotated_queryset
                   })
