70db78aa70744ac8fe7346b1c26b5a277037dd2f
Justin Tervay <jtervay97@gmail.com>
2016-06-22 19:30:05 -0400

    Add team award wins, blue banners leaderboard pages

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^leaderboard/teams/matches/$', views.team_match_wins, name='team_matches'),
     url(r'^leaderboard/teams/events/$', views.team_event_wins, name='team_events'),
     url(r'^leaderboard/teams/winrate/$', views.team_win_rate, name='team_winrate'),
+    url(r'^leaderboard/teams/awards/$', views.team_award_wins, name='team_awards'),
+    url(r'^leaderboard/teams/banners/$', views.team_blue_banners, name='team_blue_banners'),
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -12,8 +12,8 @@ def leaderboard(request):
         'All-time Event Wins': (TeamLeaderboard.most_event_wins(1).first(), reverse('team_events')),
         'All-time Highest Win Rate': (TeamLeaderboard.highest_win_rate(1).first(), reverse('team_winrate')),
         'All-time Elo Leader': (TeamLeaderboard.highest_elo_scaled(1).first(), reverse('team_elo')),
-        'All-time Award Wins': (TeamLeaderboard.most_award_wins(1).first(), ''),
-        'All-time Blue Banners': (TeamLeaderboard.most_blue_banners(1).first(), ''),
+        'All-time Award Wins': (TeamLeaderboard.most_award_wins(1).first(), reverse('team_awards')),
+        'All-time Blue Banners': (TeamLeaderboard.most_blue_banners(1).first(), reverse('team_blue_banners')),
     }
 
     return render(request, 'leaderboard/leaderboard.html', context={
@@ -44,4 +44,16 @@ def team_event_wins(request):
 def team_win_rate(request):
     return render(request, 'leaderboard/alltime/team/winrate.html', context={
         'team_winrate': TeamLeaderboard.highest_win_rate(DEFAULT_SHOW)
+    })
+
+
+def team_award_wins(request):
+    return render(request, 'leaderboard/alltime/team/awards.html', context={
+        'team_awards': TeamLeaderboard.most_award_wins(DEFAULT_SHOW)
+    })
+
+
+def team_blue_banners(request):
+    return render(request, 'leaderboard/alltime/team/blue_banners.html', context={
+        'team_blue_banners': TeamLeaderboard.most_blue_banners(DEFAULT_SHOW)
     })
