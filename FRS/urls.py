18890af818dbe25c69bc8d071df04009ea167484
Justin Tervay <jtervay97@gmail.com>
2016-09-08 20:02:13 -0400

    Re-implemented auto-redirect for 1 search result. Added 0 result redirect error page.

--- FRS/urls.py
+++ FRS/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     url(r'^', include('leaderboard.urls')),
     url(r'^', include('api.urls')),
     url(r'^$', views.landing, name="landing"),
-    url(r'^s/', views.search, name="search")
+    url(r'^s/', views.search, name="search"),
+    url(r'^s2/', views.search_api, name='search_api'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- FRS/views.py
+++ FRS/views.py
@@ -1,9 +1,10 @@
 from django.core.serializers import serialize
 from django.db.models import Q
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from TBAW.models import Team, Event
+from util.getters import reverse_model_url
 
 
 def landing(request):
@@ -11,6 +12,20 @@ def landing(request):
 
 
 def search(request):
+    query = request.GET['search']
+
+    teams = Team.objects.filter(Q(nickname__icontains=query) | Q(key__icontains=query))
+    events = Event.objects.filter(Q(key__icontains=query) | Q(name__icontains=query) | Q(short_name__icontains=query))
+
+    if teams.count() > 0:
+        return HttpResponseRedirect(reverse_model_url(teams.first()))
+    elif events.count() > 0:
+        return HttpResponseRedirect(reverse_model_url(events.first()))
+    else:
+        return render(request, 'FRS/search_found_nothing.html', {'query': query})
+
+
+def search_api(request):
     query = request.GET['search']
     team_fields = ('nickname',)
     event_fields = ('event_code',)
