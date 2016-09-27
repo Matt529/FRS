7f18193ec4e2202b431a6ca10c266e3e8053d7e5
Justin Tervay <jtervay97@gmail.com>
2016-09-27 14:53:28 -0400

    Leaderboard overview now shows leader(s) and their leading values

--- leaderboard2/models.py
+++ leaderboard2/models.py
@@ -16,12 +16,21 @@ class Leaderboard(PolymorphicModel):
     def get_models(self):
         return
 
-    def get_leaderboard_tuples(self):
-        selector = self.field[self.field.startswith('-'):]
-        return [(m, getattr(m, selector)) for m in self.get_models().order_by(self.field)]
+    def get_selector(self):
+        return self.field[self.field.startswith('-'):]
 
-    def get_leader(self):
-        return self.get_models().order_by(self.field).first()
+    def get_leaderboard_tuples(self):
+        return [(m, getattr(m, self.get_selector())) for m in self.get_models().order_by(self.field)]
+
+    def get_leaders(self):
+        leaders = self.get_models().order_by(self.field)
+        top_value = getattr(leaders.first(), self.get_selector())
+        top_leaders = [t for t in leaders if getattr(t, self.get_selector()) == top_value]
+
+        return top_leaders
+
+    def get_leading_value(self):
+        return getattr(self.get_models().order_by(self.field).first(), self.get_selector())
 
 
 class TeamLeaderboard(Leaderboard):
