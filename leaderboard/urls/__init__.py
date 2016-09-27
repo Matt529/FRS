bb885fac0ca647094b9ffad140c777d6e1bee298
Justin Tervay <jtervay97@gmail.com>
2016-09-27 14:31:08 -0400

    Changed leaderboard url on homepage to new URL. Fixed linked to CSS on base template.

--- leaderboard/urls/__init__.py
+++ leaderboard/urls/__init__.py
@@ -2,4 +2,4 @@ from .urls import teams, alliances
 from .urls_2016 import *
 from leaderboard import views
 
-urlpatterns = [url(r'^leaderboard/$', views.leaderboard, name='leaderboard')] + teams + alliances + urls_2016
+urlpatterns = [url(r'^leaderboard/$', views.leaderboard, name='deprecated_leaderboard')] + teams + alliances + urls_2016
