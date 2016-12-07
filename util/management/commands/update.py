c5f6921b1d2c45cf47cd9f9cd8b95d2b17b96607
Justin Tervay <jtervay97@gmail.com>
2016-12-07 15:23:54 -0500

    Temporary bugfix with match winrates

--- util/management/commands/update.py
+++ util/management/commands/update.py
@@ -1,11 +1,29 @@
 from django.core.management.base import BaseCommand
 
 from FRS.settings import LEADERBOARD_COUNT
-from TBAW.models import Team
+from TBAW.models import Team, Event
 from leaderboard2.models import TeamLeaderboard, ScoringLeaderboard2015, ScoringLeaderboard2016
 from util import generators
 from util.getters import get_instance_scoring_model
-from util.management.commands.addmatches import handle_event_winners
+
+
+def event_stats_temp():
+    excluded_events = ['cmp', 'cur', 'arc', 'carv', 'cars', 'gal', 'hop', 'new', 'tes', 'iri']
+    teams = Team.objects.all()
+    for team in teams:
+        num_events = Event.objects.exclude(event_code__in=excluded_events).filter(teams=team).count()
+        num_event_victories = Event.objects.exclude(event_code__in=excluded_events).filter(
+            winning_alliance__teams=team).count()
+
+        if num_events == 0:
+            continue
+
+        winrate = num_event_victories / num_events
+        team.event_attended_count = num_events
+        team.event_wins_count = num_event_victories
+        team.event_winrate = winrate
+
+    Team.objects.bulk_update(teams, update_fields=['event_attended_count', 'event_wins_count', 'event_winrate'])
 
 
 def populate_team_leaderboards():
@@ -36,8 +54,9 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        Team.objects.all().update(event_winrate=0.0, event_wins_count=0, match_winrate=0.0)
-        handle_event_winners()
-        # generators.event_win_streaks()
+        # Team.objects.all().update(event_winrate=0.0, event_wins_count=0, match_winrate=0.0)
+        # handle_event_winners()
+        event_stats_temp()  # Event winners is broken and I don't know why. temporary rewrite. :)
+        generators.event_win_streaks()
         populate_team_leaderboards()
-        # populate_scoring_leaderboards()
+        populate_scoring_leaderboards()
